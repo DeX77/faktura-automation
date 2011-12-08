a430cdeb4c69df1de7d86e4eabfd1a8dc42d2926
Daniel Exner <dex@dragonslave.de>
2011-12-08 23:52:35 +0100

    * Bugfixing * fixes #3 * fixes #4

--- cao_faktura_automation.rb
+++ cao_faktura_automation.rb
@@ -5,18 +5,21 @@ require 'mysql2'
 require 'pp'
 require 'optiflag'
 
-module DBConnection extend OptiFlagSet
-  
+
+module DBConnection
+  extend OptiFlagSet
+
   flag "db" do
     description "Database name for database connection. No default"
     long_form "database-name"
   end
-  
+
   flag "kn" do
     description "KundenNr for new created items"
     long_form "kundennummer"
+    value_matches ["KundenNr must be a number", /^\d+$/]
   end
-    
+
   optional_flag "H" do
     description "Hostname for database connection. Defaults to localhost"
     default "localhost"
@@ -28,53 +31,54 @@ module DBConnection extend OptiFlagSet
     default "root"
     long_form "username"
   end
-    
+
   optional_flag "p" do
     description "Password for database connection. Defaults to empty"
     default ""
     long_form "password"
   end
-  
+
   optional_flag "uf" do
     description "Name of the userfield for article connection. Defaults to USERFELD_01"
     default "USERFELD_01"
     long_form "userfield-name"
   end
-  
+
   optional_switch_flag "dr" do
     description "Dryrun. Don't actually write into the db"
     long_form "dry-run"
   end
-  
+
   optional_switch_flag "d" do
     description "Debug. Output some Debug infos."
     long_form "debug"
   end
-  
-  usage_flag "h","help","?"
+
+  usage_flag "h", "help", "?"
 
   and_process!
-  
+
 end
+
 
 def value_join(array)
   out = ""
-  
+
   array.each do |x|
-    
+
     #Stringbereinigung
     if x.is_a?(String) && !x.include?("()")
       out +="\'#{x}\'"
     else
       out += x.to_s
     end
-    
+
     out +=","
-    
+
   end
-  
+
   out.chop!
-  
+
   return out
 end
 
@@ -82,7 +86,7 @@ end
 def auftragsliste(client_connection, verknuepfungsfeld)
 
   auftraege = client_connection.query(
-    "select auftrag.* from JOURNAL as auftrag,
+      "select auftrag.* from JOURNAL as auftrag,
     JOURNALPOS as liste,
     ARTIKEL as artikel
     where auftrag.REC_ID = liste.JOURNAL_ID
@@ -91,116 +95,127 @@ def auftragsliste(client_connection, verknuepfungsfeld)
     and artikel.MENGE_AKT < liste.MENGE
     and auftrag.QUELLE != 1
     ")
-  
+
   return auftraege
 
 end
 
 def get_art_kunde(client_connection, kundennummer)
-  
+
   query = "select * from ADRESSEN
   where KUNNUM1 = #{kundennummer}
   "
-  
-   puts query if DBConnection.flags.d?
-  
-   kunde = client_connection.query(query)
-   
-   return kunde
+
+  puts query if DBConnection.flags.d?
+
+  kunde = client_connection.query(query)
+
+  return kunde
 end
 
 def exchange_kunde(kunde, journal_eintrag)
-  
-  journal_eintrag.each do |key, value|
-    journal_eintrag[key] = kunde[key] if kunde.has_key? key 
-  end
-  
+
+  journal_eintrag.each do |key, value |
+    journal_eintrag[key] = kunde[key]  if kunde.has_key? key
+    end
+
   return journal_eintrag
 end
 
 def postenliste(client_connection, auftrag)
-  
+
   #Herraussuchen der entsprechenden Artikel aus JOURNALPOS
   query = "select * from JOURNALPOS where JOURNAL_ID = #{auftrag[:REC_ID]}
     and ARTIKELTYP!='F'
     "
-  
-    puts query  if DBConnection.flags.d?
-    
-  
+
+  puts query if DBConnection.flags.d?
+
+
   liste = client_connection.query(query)
-    
- 
+
+
   return liste
-  
+
 end
 
 def zusammengesetzer_artikel(client_connection, listen_artikel)
-  query = 
-    "select * from ARTIKEL
+  query =
+      "select * from ARTIKEL
   where ARTNUM = #{listen_artikel[:ARTNUM]}
-  "
+      "
   puts query if DBConnection.flags.d?
-  
+
   zusammengesetzer_artikel = client_connection.query(query)
-  
+
   return zusammengesetzer_artikel
 end
 
 def stuecklisten_artikel(client_connection, zusammengesetzer_artikel, verknuepfungsfeld)
-  
-  query = 
-    "select * from ARTIKEL
-    where ARTNUM = #{zusammengesetzer_artikel[verknuepfungsfeld.to_sym]}    
-    "
-    
+
+  query =
+      "select * from ARTIKEL
+    where ARTNUM = #{zusammengesetzer_artikel[verknuepfungsfeld.to_sym]}
+      "
+
   puts query if DBConnection.flags.d?
-  
-  stuecklisten_artikel = client_connection.query(query)      
-      
-   return stuecklisten_artikel
+
+  stuecklisten_artikel = client_connection.query(query)
+
+  return stuecklisten_artikel
 end
 
-def insert_posten(client_connection, posten)
-  
+def insert_posten(client_connection, posten, neuer_einkauf)
+
   #REC_ID ist primär Key!
   posten.delete :REC_ID
-  
+
   #Loesche leere Daten
-  posten.delete_if { |key, value| (value.nil? || value.to_s.empty? || (value == -1)  || (value == 0.0)) }
-  
+  posten.delete_if { |key, value| (value.nil? || value.to_s.empty? || (value == -1) || (value == 0.0)) }
+
   bestell_pos_fields = "
- PREISANFRAGE BELEGNUM ADDR_ID LIEF_ADDR_ID PROJEKT_ID REC_ID POSITION VIEW_POS WARENGRUPPE ARTIKELTYP
- ARTIKEL_ID MATCHCODE ARTNUM BARCODE LAENGE BREITE HOEHE GROESSE DIMENSION GEWICHT ME_EINHEIT PR_EINHEIT VPE MENGE
+ PREISANFRAGE BELEGNUM ADDR_ID LIEF_ADDR_ID PROJEKT_ID REC_ID POSITION VIEW_POS WARENGRUPPE ARTIKELTYP MENGE
+ ARTIKEL_ID MATCHCODE ARTNUM BARCODE LAENGE BREITE HOEHE GROESSE DIMENSION GEWICHT ME_EINHEIT PR_EINHEIT VPE
  EPREIS GPREIS RABATT1 RABATT2 RABATT3 E_RABATT_BETRAG G_RABATT_BETRAG STEUER_CODE ALTTEIL_PROZ ALTTEIL_STCODE GEGENKTO
  BEZEICHNUNG ALTTEIL_FLAG BRUTTO_FLAG STADIUM
  "
- 
- posten.delete_if { |key,value| !bestell_pos_fields.include? key.to_s }
-  
+
+  posten.delete_if { |key, value| !bestell_pos_fields.include? key.to_s }
+
   #Neuer Posten soll EKBESTELL_ID von neuer Bestellung haben  
-  posten[:EKBESTELL_ID] = "LAST_INSERT_ID()"
-  
+  posten[:EKBESTELL_ID] = neuer_einkauf
+
+  #Mengenanpassung
+  query = "select MENGE_AKT from ARTIKEL where REC_ID =#{posten[:ARTIKEL_ID]}"
+
+  vorhandene_menge = client_connection.query(query).first[:MENGE_AKT]
+
+  if vorhandene_menge < posten[:MENGE]
+    posten[:MENGE] -= vorhandene_menge
+  end
+
+  puts "posten[:MENGE]: #{posten[:MENGE]}" if DBConnection.flags.d?
+
   insert_query ="insert into EKBESTELL_POS
     (#{posten.keys.join(',')})
     VALUES(#{value_join(posten.values)})
     "
-   puts "insert_query: #{insert_query}" if DBConnection.flags.d?
-   
+  puts "insert_query: #{insert_query}" if DBConnection.flags.d?
+
   return client_connection.query(insert_query) unless DBConnection.flags.dr?
-  
+
 end
 
 def init_einkauf(client_connection, auftrag)
-  
+
   #REC_ID ist primär Key!
   auftrag.delete :REC_ID
-  
+
   #Loesche leere Daten
-  auftrag.delete_if { |key, value| (value.nil? || value.to_s.empty? || (value == -1)  || (value == 0.0)) }
-  
+  auftrag.delete_if { |key, value| (value.nil? || value.to_s.empty? || (value == -1) || (value == 0.0)) }
+
   #Loesche unbekannte Felder
-  
+
   ekbestell_fields = "
   REC_ID         
  TERM_ID        
@@ -272,104 +287,170 @@ def init_einkauf(client_connection, auftrag)
  BRUTTO_FLAG    
  MWST_FREI_FLAG
  "
- 
- auftrag.delete_if { |key, value| !ekbestell_fields.include? key.to_s }
- 
-  auftrag[:ERSTELLT] = "CURDATE()"
-          
+
+  auftrag.delete_if { |key, value| !ekbestell_fields.include? key.to_s }
+
   insert_query ="insert into EKBESTELL
     (#{auftrag.keys.join(',')})
     VALUES(#{value_join(auftrag.values)})
     "
-    
-    pp auftrag.keys if DBConnection.flags.d?
-    
-   puts "insert_query: #{insert_query}" if DBConnection.flags.d?
-   
-  return client_connection.query(insert_query)  unless DBConnection.flags.dr?
-   
+
+  pp auftrag.keys if DBConnection.flags.d?
+
+  puts "insert_query: #{insert_query}" if DBConnection.flags.d?
+
+  return client_connection.query(insert_query) unless DBConnection.flags.dr?
+
 end
 
 
 def exchange_artikel(listen_artikel, stuecklisten_artikel)
-  
-  
+
+
   listen_artikel.each do |key, value|
-  
+
     #REC_ID ist primär Key!
     next if key == :REC_ID
-    
+
     #keine leeren Felder
     next if stuecklisten_artikel[key].nil?
-    
+
     #nicht den selben Wert
     next if value == stuecklisten_artikel[key]
-  
+
     #Menge bleibt bestehen!
     next if [:MENGE].include? key
-    
+
     puts "Aendere #{key} von #{value} in #{stuecklisten_artikel[key]}" if DBConnection.flags.d?
-    
-    listen_artikel[key] = stuecklisten_artikel[key] if stuecklisten_artikel.has_key? key    
-    
+
+    listen_artikel[key] = stuecklisten_artikel[key] if stuecklisten_artikel.has_key? key
+
   end
-  
+
   #Aendere die ARTIKEL_ID
   listen_artikel[:ARTIKEL_ID] = stuecklisten_artikel[:REC_ID]
-  
+
   #Aendere die Beschreibung
   listen_artikel[:BEZEICHNUNG] = stuecklisten_artikel[:LANGNAME]
-  
+
   return listen_artikel
 end
 
-client = Mysql2::Client.new(
-  :host => DBConnection.flags.H,
-  :username => DBConnection.flags.u,
-  :database => DBConnection.flags.db,
-  :password => DBConnection.flags.p  
-)
+def copy_file_link(client_connection, journal_item_from, journal_item_to)
+
+  puts "journal_item_from"
+  pp journal_item_from
+
+  query = "select * from LINK
+  where REC_ID = #{journal_item_from[:REC_ID]}
+  "
+
+  puts query if DBConnection.flags.d?
+
+  link = client_connection.query(query)
+
+  if !link.empty?
+
+    link[:REC_ID] = journal_item_to
+
+    insert_query ="insert into LINK
+    (#{link.keys.join(',')})
+    VALUES(#{value_join(link.values)})
+    "
+
+
+    return client_connection.query(insert_query) unless DBConnection.flags.dr?
+  end
+
+
+end
+
+def backup_last_journal(client_connection)
+
+  query = "select LAST_INSERT_ID()"
+
+  puts query if DBConnection.flags.d?
+
+  last_entry = client_connection.query(query).first
+
+  if DBConnection.flags.d?
+    puts "letzter eingefuegter Eintrag"
+    pp last_entry
+  end
+
+  return last_entry.values.last
+
+end
+
+
+def init_db_connection(db)
+
+  client = Mysql2::Client.new(
+      :host => db.flags.H,
+      :username => db.flags.u,
+      :database => db.flags.db,
+      :password => db.flags.p
+  )
 
 #Symbolize keys
-client.query_options.merge!(:symbolize_keys => true)
+  client.query_options.merge!(:symbolize_keys => true)
 
-auftraege = auftragsliste(client, DBConnection.flags.uf)
+  return client
+end
 
-default_kunde = get_art_kunde(client, DBConnection.flags.kn).first
+def process_auftraege(client)
 
-puts "Anzahl zu bearbeitender Auftraege: #{auftraege.count}" if DBConnection.flags.d?
 
-auftraege.each do |auftrag|
+  auftraege = auftragsliste(client, DBConnection.flags.uf)
+
+  default_kunde = get_art_kunde(client, DBConnection.flags.kn).first
+
+  puts "Anzahl zu bearbeitender Auftraege: #{auftraege.count}" if DBConnection.flags.d?
+
+  auftraege.each do |auftrag|
+
     liste = postenliste(client, auftrag)
-    puts "Anzahl der zu bearbeitenden Posten im Auftrag #{auftrag[:VRENUM]} : #{liste.count}" if DBConnection.flags.d?
-    
+
+    auftrags_nummer = auftrag[:VRENUM]
+    puts "Anzahl der zu bearbeitenden Posten im Auftrag #{auftrags_nummer} : #{liste.count}" if DBConnection.flags.d?
+
+    selbst_auftrag = exchange_kunde(default_kunde, auftrag)
+
+    init_einkauf(client, selbst_auftrag)
+
+    neuer_einkauf = backup_last_journal(client)
+
     liste.each do |posten|
-      
+
       zusammengesetzer_artikel = zusammengesetzer_artikel(client, posten)
-      
+
       stuecklisten_artikel = stuecklisten_artikel(client, zusammengesetzer_artikel.first, DBConnection.flags.uf)
 
-      if DBConnection.flags.d? 
-	puts "stuecklisten_artikel: " 
-	pp stuecklisten_artikel.first
+      if DBConnection.flags.d?
+        puts "stuecklisten_artikel: "
+        pp stuecklisten_artikel.first
       end
 
       exchange_artikel = exchange_artikel(posten, stuecklisten_artikel.first)
 
       if DBConnection.flags.d?
-	puts "exchange_artikel: "
-	pp exchange_artikel
+        puts "exchange_artikel: "
+        pp exchange_artikel
       end
-      
-      selbst_auftrag = exchange_kunde(default_kunde, auftrag)
 
-      init_einkauf(client, selbst_auftrag)
+      insert_posten(client, exchange_artikel, neuer_einkauf)
 
-      insert_posten(client, exchange_artikel)
+    end unless neuer_einkauf.nil? #sollte eigentlich nicht passieren
 
-    end
+    #copy_file_link(client, auftrags_nummer, neuer_einkauf) unless neuer_einkauf.nil? #sollte eigentlich nicht passieren
+
+  end
 end
 
+
+client = init_db_connection(DBConnection)
+
+process_auftraege(client)
 
 
 
